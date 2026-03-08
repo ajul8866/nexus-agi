ef7af99a618b61835972fc4074f341acc0cdee13
ajultect <bizgudboy@gmail.com>
2026-03-09 06:00:28 +0800

    feat: initial NEXUS-AGI framework - complete implementation

---
+++ nexus/rsi/__init__.py
@@ -0,0 +1,6 @@
+from .monitor import PerformanceMonitor
+from .optimizer import PromptOptimizer
+from .generator import ToolGenerator
+from .improver import SelfImprover
+
+__all__ = ["PerformanceMonitor", "PromptOptimizer", "ToolGenerator", "SelfImprover"]
