f229659e5de05c4d570b1bc6648e04ad97470b51
ajultect <bizgudboy@gmail.com>
2026-03-09 06:24:22 +0800

    sync: update nexus/__init__.py from Nebula workspace

--- nexus/__init__.py
+++ nexus/__init__.py
@@ -1,2 +1,7 @@
-from .kernel import NexusKernel
-__all__ = ["NexusKernel"]
+"""NEXUS-AGI: Super AGI Framework"""
+__version__ = "0.1.0"
+__author__ = "ajul8866"
+
+from nexus.kernel import NexusKernel, AgentLifecycle, MessageBus
+
+__all__ = ["NexusKernel", "AgentLifecycle", "MessageBus", "__version__"]
